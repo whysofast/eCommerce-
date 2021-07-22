c295e5771c251bef9118dcbc6627bbbbf0779773
WhySoFast <matheusvca@gmail.com>
2021-07-21 22:10:35 -0300

    criação da migration

---
+++ src/main/resources/db/migration/V001__create_product.sql
@@ -0,0 +1,10 @@
+CREATE TABLE product
+(
+    id          varchar(50),
+    name            varchar(50),
+    description      varchar(255),
+    price       varchar(100),
+    created_at  datetime,
+    modified_at datetime,
+    PRIMARY KEY (id)
+);
---
+++ src/test/kotlin/com/example/ecommercefast/ECommerceFastApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ecommercefast
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ECommerceFastApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
